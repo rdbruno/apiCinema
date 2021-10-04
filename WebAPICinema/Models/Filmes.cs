4f277e26612f3e74cd3138c593ccc23f5ff6e45b
Lucas Silv <63755798+LucasSCSilv@users.noreply.github.com>
2021-10-03 22:44:00 -0300

    Add files via upload

---
+++ WebAPICinema/Models/Filmes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPICinema.Models
+{
+    public class Filmes
+    {
+        public int IdFilme { get; set; }
+
+        public string NomeFilme { get; set; }
+
+        public decimal PrecoIngresso { get; set; }
+    }
+}
